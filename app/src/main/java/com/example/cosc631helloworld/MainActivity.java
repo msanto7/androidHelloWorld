b80ae89be93776f643317ca7d8ce6154ff63f40f
Mike Santoro <mike.santoro42@gmail.com>
2019-07-13 12:32:42 -0400

    accepting input and return string concat

--- app/src/main/java/com/example/cosc631helloworld/MainActivity.java
+++ app/src/main/java/com/example/cosc631helloworld/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.cosc631helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String firstName, lastName, message;
+
+    EditText firstNameInput;
+    EditText lastNameInput;
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        firstNameInput = (EditText) findViewById(R.id.firstNameInput);
+        lastNameInput = (EditText) findViewById(R.id.lastNameInput);
+
+        submitButton = (Button) findViewById(R.id.submitBtn);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstName = firstNameInput.getText().toString();
+                lastName = lastNameInput.getText().toString();
+
+                message = "Hi " + firstName + " " + lastName;
+                showToast(message);
+            }
+        });
+
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
